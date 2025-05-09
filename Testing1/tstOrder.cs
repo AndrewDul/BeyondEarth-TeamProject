c6f27371d93a2b06bedee5f00ce6490cdf8e3d85
P2760131 <P2760131@my365.dmu.ac.uk>
2025-05-09 23:38:39 +0100

    Testing clsOrder

---
+++ Testing1/tstOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstOrder
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            clsOrder order = new clsOrder();
+            Assert.IsNotNull(order);
+        }
+    }
+}
