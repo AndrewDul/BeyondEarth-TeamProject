eff2c69b06132a1e1321e9816f310e1ef0533e40
P2760131 <P2760131@my365.dmu.ac.uk>
2025-05-17 21:23:06 +0100

    Added clsOrder unit tests created property tests in tstOrder

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,9 +1,28 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsOrder
     {
-        public clsOrder()
-        {
-        }
+        // Primary Key - Order ID
+        public int OrderID { get; set; }
+
+        // Foreign Key to Customer table
+        public int CustomerID { get; set; }
+
+        // Foreign Key to Product/Experience table
+        public int ProductID { get; set; }
+
+        // Date the order was placed
+        public DateTime OrderDate { get; set; }
+
+        // Total price of the order
+        public decimal TotalPrice { get; set; }
+
+        // Status of the order, e.g., Pending, Approved
+        public string OrderStatus { get; set; }
+
+        // Whether the order has been cancelled
+        public bool IsCancelled { get; set; }
     }
 }
--- Testing1/tstOrder.cs
+++ Testing1/tstOrder.cs
@@ -8,10 +8,76 @@ namespace Testing1
     public class tstOrder
     {
         [TestMethod]
-        public void TestMethod1()
+        public void InstanceOK()
         {
-            clsOrder order = new clsOrder();
-            Assert.IsNotNull(order);
+            // Create an instance of clsOrder
+            clsOrder AnOrder = new clsOrder();
+            // Test that it exists
+            Assert.IsNotNull(AnOrder);
+        }
+
+        [TestMethod]
+        public void OrderIDPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 1;
+            AnOrder.OrderID = TestData;
+            Assert.AreEqual(AnOrder.OrderID, TestData);
+        }
+
+        [TestMethod]
+        public void CustomerIDPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 101;
+            AnOrder.CustomerID = TestData;
+            Assert.AreEqual(AnOrder.CustomerID, TestData);
+        }
+
+        [TestMethod]
+        public void ProductIDPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            int TestData = 202;
+            AnOrder.ProductID = TestData;
+            Assert.AreEqual(AnOrder.ProductID, TestData);
+        }
+
+        [TestMethod]
+        public void OrderDatePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            DateTime TestData = DateTime.Now.Date;
+            AnOrder.OrderDate = TestData;
+            Assert.AreEqual(AnOrder.OrderDate, TestData);
+        }
+
+        [TestMethod]
+        public void TotalPricePropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            decimal TestData = 99.99m;
+            AnOrder.TotalPrice = TestData;
+            Assert.AreEqual(AnOrder.TotalPrice, TestData);
+        }
+
+        [TestMethod]
+        public void OrderStatusPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            string TestData = "Pending";
+            AnOrder.OrderStatus = TestData;
+            Assert.AreEqual(AnOrder.OrderStatus, TestData);
+        }
+
+        [TestMethod]
+        public void IsCancelledPropertyOK()
+        {
+            clsOrder AnOrder = new clsOrder();
+            bool TestData = true;
+            AnOrder.IsCancelled = TestData;
+            Assert.AreEqual(AnOrder.IsCancelled, TestData);
         }
     }
+
 }
