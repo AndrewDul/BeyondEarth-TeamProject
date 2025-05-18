5edef35cd39072075106ea34694ba44982870b87
P2760131 <P2760131@my365.dmu.ac.uk>
2025-05-18 17:12:05 +0100

    display order status

---
+++ AdminSystem/OrderDataEntry.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary;
+
+public partial class _1_DataEntry : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void txtOrderID_TextChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsOrder
+        clsOrder AnOrder = new clsOrder();
+
+        //capture orderStatus
+        AnOrder.OrderStatus= txtOrderStatus.Text;
+
+        //store the orders in the session object
+        Session["AnOrder"] = AnOrder;
+
+        //navigate to the view page
+        Response.Redirect("OrderViewer.aspx");
+    }
+}
---
+++ AdminSystem/OrderViewer.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary;
+
+public partial class _1Viewer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       // create a new instance of clsOrder
+       clsOrder AnOrder = new clsOrder();
+
+        //get the data from the session object
+        AnOrder = (clsOrder)Session["Anorder"];
+
+        // Display the Order Status for this entry
+        Response.Write(AnOrder.OrderStatus);
+    }
+}
