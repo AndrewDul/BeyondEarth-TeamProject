3e4b595bb5b5499c5e7e39b2b6d544e65ef5e8fb
AmerOmar <P2755691@ERWX102AWIN151W>
2025-05-20 19:36:49 +0100

    Created FIND methods
    Added different tests for every property
    
    made tests on all and everything works great
    
    created private members
    
    created getters and setters
    
    Changes made by Amer Bamazrua

--- ClassLibrary/clsManagment.cs
+++ ClassLibrary/clsManagment.cs
@@ -4,13 +4,72 @@ namespace ClassLibrary
 {
     public class clsManagment
     {
-        public bool Active { get; set; }
-        public DateTime DateAdded { get; set; }
-        public int CustomerId { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string PostCode { get; set; }
-        public string PhoneNumber { get; set; }
-        public string EmailAddress { get; set; }
+        // Private data members
+        private Int32 mCustomerId;
+        private string mFirstName;
+        private string mLastName;
+        private string mEmailAddress;
+        private string mPostCode;
+        private string mPhoneNumber;
+        private bool mActive;
+        private DateTime mDateAdded;
+
+        // Public properties
+        public bool Active
+        {
+            get { return mActive; }
+            set { mActive = value; }
+        }
+        public DateTime DateAdded
+        {
+            get { return mDateAdded; }
+            set { mDateAdded = value; }
+        }
+        public Int32 CustomerId
+        {
+            get { return mCustomerId; }
+            set { mCustomerId = value; }
+        }
+        public string FirstName
+        {
+            get { return mFirstName; }
+            set { mFirstName = value; }
+        }
+        public string LastName
+        {
+            get { return mLastName; }
+            set { mLastName = value; }
+        }
+        public string PostCode
+        {
+            get { return mPostCode; }
+            set { mPostCode = value; }
+        }
+        public string PhoneNumber
+        {
+            get { return mPhoneNumber; }
+            set { mPhoneNumber = value; }
+        }
+        public string EmailAddress
+        {
+            get { return mEmailAddress; }
+            set { mEmailAddress = value; }
+        }
+
+        public bool Find(int customerId)
+        {
+            // Set hardcoded values for testing purposes
+            mCustomerId = 21;
+            mFirstName = "Amer";
+            mLastName = "Bamazrua";
+            mEmailAddress = "john.doe@example.com";
+            mPostCode = "LE1 1AA";
+            mPhoneNumber = "0116 123 4567";
+            mActive = true;
+            mDateAdded = Convert.ToDateTime("2022-12-23");
+
+            //always return true
+            return true;
+        }
     }
 }
