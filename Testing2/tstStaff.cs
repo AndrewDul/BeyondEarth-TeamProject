488a19f36c9e06411cd870af273c46da91817fdc
P2805200 <P2805200@GWHXX354WIN014W>
2025-05-21 10:35:51 +0100

    Created Staff class and test.

---
+++ Testing2/tstStaff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            return "";
+        }
+    }
+}
