952a203085241bb987c8f1ca889e700b4e5dc4fd
P2760131 <P2760131@my365.dmu.ac.uk>
2025-05-21 21:22:51 +0100

    Create clsOrderCollection class and unit test file

---
+++ Testing1/tstOrderCollection.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing1
+{
+    [TestClass]
+    public class tstOrderCollection
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // Create an instance of the collection class
+            clsOrderCollection AllOrders = new clsOrderCollection();
+            // Test to see that it exists
+            Assert.IsNotNull(AllOrders);
+        }
+
+        
+    }
+}
