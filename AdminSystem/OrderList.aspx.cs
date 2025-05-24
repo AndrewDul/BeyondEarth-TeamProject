2dd8405b97726320e0865320c34c71799b1878bf
P2760131 <P2760131@my365.dmu.ac.uk>
2025-05-24 19:22:30 +0100

    Adding the Presentation Layer

---
+++ AdminSystem/OrderList.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary;
+
+public partial class _1_List : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // if this is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            // update the list box
+            DisplayOrders();
+        }
+    }
+
+    void DisplayOrders()
+    {
+        // create an instance of the Order collection
+        clsOrderCollection Orders = new clsOrderCollection();
+
+        // set the data source to the list of orders in the collection
+        lstOrderList.DataSource = Orders.OrderList;
+
+        // set the name of the primary key (hidden value)
+        lstOrderList.DataValueField = "OrderID";
+
+        // set the field to display in the list box (you can change it if needed)
+        lstOrderList.DataTextField = "OrderStatus";
+
+        // bind the data to the list box
+        lstOrderList.DataBind();
+    }
+}
