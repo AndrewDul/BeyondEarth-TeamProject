0635976485c30a7c5f23f885ffd0af4388170087
ahmadrmfc <157719147+ahmadrmfc@users.noreply.github.com>
2025-06-13 06:17:07 +0100

    Create 4stockViewer.aspx.cs

---
+++ AdminSystem/4stockViewer.aspx.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClassLibrary;
+
+public partial class _1Viewer : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        clsStock AStock = new clsStock();
+        AStock = (clsStock)Session["AStock"];
+        Response.Write(AStock.StockName);
+    }
+}
