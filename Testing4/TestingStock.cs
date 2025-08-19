7f3c28ca80c208e065f430790057709e7c1d6c61
ahmadrmfc <157719147+ahmadrmfc@users.noreply.github.com>
2025-08-20 00:02:35 +0100

    Add files via upload
    added  some tests

---
+++ Testing4/TestingStock.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStock
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            // Create an instance of the class we want to create
+            clStock AStock = new clStock();
+
+            // Test to see that it exists
+            Assert.IsNotNull(AStock);
+        }
+    }
+}
---
+++ Testing4/clsStock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Remoting.Messaging;
+
+namespace Testing4
+{
+    internal class clsStock
+    {
+        public clsStock()
+        {
+        }
+
+        public int StockSellingPrice { get; internal set; }
+        public int? QuantityInStock { get; internal set; }
+        public string StockStatus { get; internal set; }
+        public DateTime StockExpiryDate { get; internal set; }
+        public string StockName { get; internal set; }
+        public string StockCode { get; internal set; }
+        public DateTime DateAdded { get; internal set; }
+        public bool Active { get; internal set; }
+
+       
+internal string Valid(int stockId, string stockName, string stockCode, int stockSellingPrice, int quantityInStock, string stockStatus, DateTime stockExpiryDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string Valid(int stockId, string stockName, int stockSellingPrice, int quantityInStock, string stockStatus, DateTime stockExpiryDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    }
